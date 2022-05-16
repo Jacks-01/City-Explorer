441ff09fd94dc4f84561830283fc3317f8a055ac
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-16 13:46:54 -0500

    installed axios and started app.js

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+/**
+ * @file App.js
+ * @author Jack Stubblefield
+ * @description City Explorer: retrieves data from the locationIQ API and displays the location entered on the web page.
+ */
+import React, { Component } from 'react';
