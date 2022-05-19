8725e8b896488b376747a2ea88627214c1fdefc9
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-19 16:10:33 -0500

    formatted main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,30 +2,15 @@ import React, { Component } from 'react';
 import CitySearch from './CitySearch';
 import Weather from './Weather';
 
-
-
 class Main extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			
-		};
-        
-           
-	} 
-    render() { 
-        return (
-            <div>
-            <h1>Hi this is bob</h1>
-            <CitySearch/>
-            <Weather/>
-            </div>
-          );
-    } 
-    /**
-     * <CitySelector/>
-            <Weather/>
-     */
+	render() {
+		return (
+			<div>
+				<CitySearch />
+				<Weather />
+			</div>
+		);
+	}
 }
- 
+
 export default Main;
