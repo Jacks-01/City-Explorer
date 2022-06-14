08f221392dc01ccff9ccc888f355ca8ff084fdd0
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-14 14:58:34 -0500

    fixed deploy

--- src/components/CityData.js
+++ src/components/CityData.js
@@ -35,7 +35,7 @@ class CityData extends Component {
 	callBackendAPI = async () => {
 		await axios({
 			method: 'get',
-			url: `${server}weather`,
+			url: `https://city-explorer-jack.herokuapp.com/weather`,
 			params: {
 				city: this.state.searchQuery,
 				lat: this.state.location.lat,
@@ -48,7 +48,7 @@ class CityData extends Component {
 
 		await axios({
 			method: 'get',
-			url: `${server}movies`,
+			url: `https://city-explorer-jack.herokuapp.com/movies`,
 			params: {
 				year: 2022,
 			},
