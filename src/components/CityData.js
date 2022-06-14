f35acf452f4b5fadb70d8786e76172b37248259d
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-14 14:26:33 -0500

    removed console logs from main

--- src/components/CityData.js
+++ src/components/CityData.js
@@ -41,72 +41,60 @@ class CityData extends Component {
 				lon: this.state.location.lon,
 			},
 		}).then((response) => {
-			// console.log(response.data);
 			this.setState({ weatherData: response.data });
-			// console.log(this.weatherData);
 			this.setState({ city: this.state.location.display_name });
 		});
-		
+
 		await axios({
 			method: 'get',
 			url: 'http://localhost:5000/movies',
 			params: {
-				year: 2022
+				year: 2022,
 			},
 		})
-		.then((response) => {
-			// console.log(response);
-			this.setState({movieData: response.data});
-			// console.log('current state of movieData:', this.state.movieData);
-		}).catch((err) => {
-			console.error(err);
-		});
+			.then((response) => {
+				this.setState({ movieData: response.data });
+			})
+			.catch((err) => {
+				console.error(err);
+			});
 	};
 
 	getLocation = async (cityName) => {
 		try {
-			console.log('this is the city', cityName);
 			this.setState({ searchQuery: cityName });
 			const API = `https://us1.locationiq.com/v1/search.php?key=${locationKey}&q=${cityName}&format=json`;
 			const res = await axios.get(API);
-			// console.log('this is the locationIQ data',res.data);
 			this.setState({ location: res.data[0] }, this.getMap);
 			this.callBackendAPI();
-			// console.log('this is the state of location:', this.state);
 		} catch (error) {
 			console.error(error);
 			this.setState({ show: true });
 		}
-
 	};
 
 	getMap = async () => {
-			const API = `https://maps.locationiq.com/v3/staticmap?key=${locationKey}&center=${this.state.location.lat},${this.state.location.lon}&zoom=18&size=500x500&format=png`;
-			this.setState({ map: API });
+		const API = `https://maps.locationiq.com/v3/staticmap?key=${locationKey}&center=${this.state.location.lat},${this.state.location.lon}&zoom=18&size=500x500&format=png`;
+		this.setState({ map: API });
 	};
-	
 
 	render() {
-		console.log(this.state.weatherData);
 		return (
 			<>
-				<CityForm getLocation={this.getLocation}/>
+				<CityForm getLocation={this.getLocation} />
 				<CityMap
-						location={this.state.location}
-						map={this.state.map}
-						show={this.state.show}
-						error={this.state.error}
-					/>
+					location={this.state.location}
+					map={this.state.map}
+					show={this.state.show}
+					error={this.state.error}
+				/>
 				{this.state.city && (
-					<>
+					<section>
 						<h2>Your 3-day forecast for {this.state.city} is: </h2>
-						<Weather weatherData={this.state.weatherData}/>
-					
-					</>
+						<Weather weatherData={this.state.weatherData} />
+					</section>
 				)}
-				{this.state.weatherData && (
-					<Movies movieData={this.state.movieData}/>	
-				)}
+				{this.state.weatherData && <Movies movieData={this.state.movieData} />}
 			</>
 		);
 	}
