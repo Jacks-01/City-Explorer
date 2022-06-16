36ccb7250762c9f137fa1f004cef9f26a4b651f4
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-16 12:11:41 -0500

    created bootstrap cards for weather items

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,25 +3,18 @@
  * @description displays weather data in a nice bootstrap component
  */
 import React, { Component } from 'react';
-import { Image } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 // C:\Users\DeltaVStudent\projects\Code301\city-explorer\city-explorer-front\src\icons\._c03d.png
 const Weather = (props) => {
 	return props.weatherData.map((day, index) => (
-		<div key={index}>
-			<Image src={`/icons/${day.icon}.png`}/>
-			<p>{day.date}</p>
-			<p>{day.description}</p>
-		</div>
+			<Card key={index} style={{width: '12rem'}}>
+				<Card.Img variant='top' src={`/icons/${day.icon}.png`}/>
+				<Card.Body>
+					<Card.Title>{day.date}</Card.Title>
+					<Card.Text>{day.description}</Card.Text>
+				</Card.Body>
+			</Card>
 	));
 };
 
-class WeatherDay extends Component {
-	render() { 
-		return (
-			<>
-			</>
-		);
-	}
-}
- 
 export default Weather;
