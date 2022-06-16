a4a7d922e2a18b442dd6c6557dc7b5d92ae0813a
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-16 10:56:26 -0500

    added icons folder

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,6 +2,7 @@
  * @file Weather.js
  * @description displays weather data in a nice bootstrap component
  */
+import React, { Component } from 'react';
 
 const Weather = (props) => {
 	return props.weatherData.map((day, index) => (
@@ -12,4 +13,13 @@ const Weather = (props) => {
 	));
 };
 
+class WeatherDay extends Component {
+	render() { 
+		return (
+			<>
+			</>
+		);
+	}
+}
+ 
 export default Weather;
